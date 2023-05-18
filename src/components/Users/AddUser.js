88d1f855ba8d070bb9dbeff44a99ecd83b5453ec
Kevin Tursi <kevin.tursi@uconn.edu>
2023-05-18 16:08:28 -0400

    Added Card.js in UI folder and css modules

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label>Username</label>
-            <input id="username" htmlFor="username" type="text" />
-            <label>Age (Years)</label>
-            <input id="age" htmlFor="age" type="number" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card  className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label>Username</label>
+                <input id="username" htmlFor="username" type="text" />
+                <label>Age (Years)</label>
+                <input id="age" htmlFor="age" type="number" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 
